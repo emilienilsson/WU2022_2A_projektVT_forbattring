3fdccaef422b5093f1e6ec6d03287cf6bbbf9ba2
emilienilsson <emilie.nilsson@elev.ga.ntig.se>
2022-05-28 15:55:31 +0200

    Fixat class och id och stylat med media, samt försökt lägga till en till funktion i js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 const nav = document.getElementById("nav-links")
 const burger = document.getElementById("burger")
+const main = document.getElementById("??")
  
 function toggleMenu() {
     nav.classList.toggle("nav-active")
     burger.classList.toggle("crossed_line")
+    main.classList.toggle("hide-item")
 }
  
 burger.addEventListener("click", toggleMenu)
+
+
